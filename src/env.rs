650a3655887aea4da5f8b2ee7a3f888509907964
Zoburo <me@zoburo.com>
2026-07-11 13:18:23 +0700

    feat: remove service env vars

--- src/env.rs
+++ src/env.rs
@@ -9,12 +9,6 @@ pub struct Env {
 
     #[envconfig(from = "HTTP_SERVER_URL", default = "/")]
     pub http_server_url: String,
-
-    #[envconfig(from = "CARGO_PKG_NAME")]
-    pub service_name: String,
-
-    #[envconfig(from = "CARGO_PKG_VERSION")]
-    pub service_version: String,
 }
 
 /// Load environment configuration from `.env` file and initialize logging
